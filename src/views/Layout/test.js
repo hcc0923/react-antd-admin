2a56584bf947ac44ce2f9d0a61abaeed5f572a2e
hcc960923 <101438085+hcc960923@users.noreply.github.com>
2022-06-28 17:30:24 +0800

    111

---
+++ src/views/Layout/test.js
@@ -0,0 +1,19 @@
+import {
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+    UploadOutlined,
+    UserOutlined,
+    VideoCameraOutlined,
+  } from '@ant-design/icons';
+  import { Layout, Menu } from 'antd';
+  import React, { useState } from 'react';
+  const { Header, Sider, Content } = Layout;
+  
+  const App = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    return (
+
+    );
+  };
+  
+  export default App;
