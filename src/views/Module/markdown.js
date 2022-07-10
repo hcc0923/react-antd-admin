aefe0891a3934d11c4d68bb5d0c3a5f9ccf58735
hcc <hcc960923@gmail.com>
2022-07-10 21:19:15 +0800

    ccc

--- src/views/Module/markdown.js
+++ src/views/Module/markdown.js
@@ -1,6 +1,5 @@
 // import React, { Component } from 'react';
 // import Editor from 'for-editor'
-// import './markdown.less';
 
 
 // class MarkDown extends Component {
--- src/views/Module/rich-text.js
+++ src/views/Module/rich-text.js
@@ -1,96 +1,79 @@
-// import React, { Component } from 'react';
-// import { Row, Col, Card } from 'antd';
-// import { EditorState } from 'draft-js';
-// import { Editor } from 'react-draft-wysiwyg';
-// import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-// import draftToHtml from 'draftjs-to-html';
-// import draftToMarkdown from 'draftjs-to-markdown';
-// import './richtext.less';
-// import { SERVER_ADDRESS } from '@/utils/config';
+import React, { Fragment, Component, useState } from 'react';
+import { Row, Col, Card } from 'antd';
+import { EditorState } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import draftToHtml from 'draftjs-to-html';
+import draftToMarkdown from 'draftjs-to-markdown';
+import { SERVER_ADDRESS } from '@/utils/config';
 
 
-// class RichText extends Component {
-//     state = { 
-//         editorContent: undefined,
-//         editorState: EditorState.createEmpty() 
-//     };
-//     onEditorStateChange = (editorState) => {
-//         this.setState({
-//             editorState,
-//         });
-//     };
-//     onContentStateChange = (editorContent) => {
-//         this.setState({
-//             editorContent
-//         });
-//     };
-//     imageUploadCallBack = (file) => {
-//         console.log(file);
-//         return new Promise((resolve, reject) => {
-//             const xhr = new XMLHttpRequest();
-//             xhr.open('POST', `${SERVER_ADDRESS}/file/uploadAvatar`);
+function RichText() {
+    const [editorContent, setEditorContent] = useState(null);
+    const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
+    const onEditorStateChange = (editorState) => {
+        setEditorState(editorState);
+    }
+    const onContentStateChange = (editorContent) => {
+        setEditorContent(editorContent);
+    }
+    const imageUploadCallBack = (file) => {
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open('POST', `${SERVER_ADDRESS}/file/uploadAvatar`);
             
-//             const formData = new FormData();
-//             formData.append('image', file);
-//             xhr.send(formData);
+            const data = new FormData();
+            data.append('image', file);
+            xhr.send(data);
 
-//             xhr.addEventListener('load', () => {
-//                 const response = JSON.parse(xhr.responseText);
-//                 resolve(response);
-//             });
-//             xhr.addEventListener('error', () => {
-//                 const error = JSON.parse(xhr.responseText);
-//                 reject(error);
-//             });
-//         });
-//     };
-//     render() { 
-//         const { editorContent, editorState } = this.state;
-//         return (  
-//             <div className="editor">
-//                 <Row>
-//                     <Col span={24}>
-//                         <Card title="富文本编辑器">
-//                             <Editor
-//                                 editorState={editorState}
-//                                 toolbarClassName="home_toolbar"
-//                                 wrapperClassName="wrapperClassName"
-//                                 editorClassName="home_editor"
-//                                 onEditorStateChange={this.onEditorStateChange}
-//                                 toolbar={{
-//                                     history: { inDropdown: true },
-//                                     inline: { inDropdown: false },
-//                                     list: { inDropdown: true },
-//                                     textAlign: { inDropdown: true },
-//                                     image: { uploadCallback: this.imageUploadCallBack }
-//                                 }}
-//                                 onContentStateChange={this.onContentStateChange}
-//                                 placeholder="请输入正文"
-//                                 spellCheck
-//                                 style={{minHeight: '350px'}}
-//                             />
-//                         </Card>
-//                     </Col>
-//                 </Row>
-//                 <Row gutter={8} style={{marginTop: "10px"}}>
-//                     <Col span={8}>
-//                         <Card title="同步转换HTML" hoverable="true" bordered={false}>
-//                             <pre>{draftToHtml(editorContent)}</pre>
-//                         </Card>
-//                     </Col>
-//                     <Col span={8}>
-//                         <Card title="同步转换MarkDown" hoverable="true"  bordered={false}>
-//                             <pre style={{ whiteSpace: 'pre-wrap' }}>{draftToMarkdown(editorContent)}</pre>
-//                         </Card>
-//                     </Col>
-//                     <Col span={8}>
-//                         <Card title="同步转换JSON" hoverable="true"  bordered={false}>
-//                             <pre style={{ whiteSpace: 'normal' }}>{JSON.stringify(editorContent)}</pre>
-//                         </Card>
-//                     </Col>
-//                 </Row>
-//             </div>
-//         );
-//     };
-// };
-// export default RichText;
+            xhr.addEventListener('load', () => {
+                const response = JSON.parse(xhr.responseText);
+                resolve(response);
+            });
+            xhr.addEventListener('error', () => {
+                const error = JSON.parse(xhr.responseText);
+                reject(error);
+            });
+        });
+    }
+
+    return (  
+        <Fragment>
+            <Row>
+                <Col span={24}>
+                    <Card title="富文本编辑器" bordered>
+                        <Editor
+                            editorState={editorState}
+                            onEditorStateChange={(value) => onEditorStateChange(value)}
+                            toolbar={{
+                                history: { inDropdown: true },
+                                inline: { inDropdown: false },
+                                list: { inDropdown: true },
+                                textAlign: { inDropdown: true },
+                                image: { uploadCallback: (file) => imageUploadCallBack(file) }
+                            }}
+                            onContentStateChange={(value) => onContentStateChange(value)}
+                            placeholder="请输入正文"
+                            spellCheck
+                        />
+                    </Card>
+                </Col>
+            </Row>
+            <Row gutter={12} className="mt-4">
+                <Col span={12}>
+                    <Card title="同步转换HTML" hoverable="true" bordered={false}>
+                        <pre>{draftToHtml(editorContent)}</pre>
+                    </Card>
+                </Col>
+                <Col span={12}>
+                    <Card title="同步转换Markdown" hoverable="true" bordered={false}>
+                        <pre style={{ whiteSpace: 'pre-wrap' }}>{draftToMarkdown(editorContent)}</pre>
+                    </Card>
+                </Col>
+            </Row>
+        </Fragment>
+    );
+}
+
+export default RichText;
