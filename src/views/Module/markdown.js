e5880fd2baeeea3d18cd623c8a73f40dea648a8c
hcc960923 <101438085+hcc960923@users.noreply.github.com>
2022-07-11 15:11:06 +0800

    ccc

--- src/views/Module/markdown.js
+++ src/views/Module/markdown.js
@@ -1,28 +1,11 @@
-// import React, { Component } from 'react';
-// import Editor from 'for-editor'
+import React, { useState, useEffect } from 'react';
 
 
-// class MarkDown extends Component {
-//     state = { 
-//         value: ''
-//     };
-//     handleChange = (value) => {
-//         this.setState({
-//             value
-//         });
-//     };
-//     render() { 
-//         const { value } = this.state;
-//         return (  
-//             <div className="markdown">
-//                 <Editor 
-//                     value={value} 
-//                     placeholder="开始编辑"
-//                     height="500px"
-//                     onChange={this.handleChange} 
-//                 />
-//             </div>
-//         );
-//     };
-// };
-// export default MarkDown;
+function MarkDown() {
+
+    return (
+        <div id="editorSection"></div>
+    );
+}
+
+export default MarkDown;
