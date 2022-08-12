59817338f78c5ddc9c7ceb2ee85829b26bbcc4aa
hcc960923 <101438085+hcc960923@users.noreply.github.com>
2022-08-12 17:34:15 +0800

    ccc

--- server/user.sql
+++ server/user.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50738
  File Encoding         : 65001
 
- Date: 10/08/2022 10:01:30
+ Date: 12/08/2022 17:33:54
 */
 
 SET NAMES utf8mb4;
@@ -28,16 +28,19 @@ CREATE TABLE `file`  (
   `time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0),
   `userId` int(11) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 20 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of file
 -- ----------------------------
-INSERT INTO `file` VALUES (1, 'profile-fb-kid-02.png', '944cc31907adde46737737c10f29106f.png', '2022-08-10 10:00:27', 4);
-INSERT INTO `file` VALUES (2, 'profile-fb-kid-03.png', 'f680eb8c754391b887fe91181e6962d5.png', '2022-08-10 10:00:27', 4);
-INSERT INTO `file` VALUES (3, 'profile-fb-kid-04.png', '7e9b832c66ec15817271145821c91b79.png', '2022-08-10 10:00:27', 4);
-INSERT INTO `file` VALUES (4, '全部信息 (1).xlsx', 'b70c4b1b5e2c5be4d21036a3f7c900be.xlsx', '2022-08-10 10:00:42', 4);
-INSERT INTO `file` VALUES (5, '20220705-093120.jpg', '92cdf867d5bc1524d110b21316179922.jpg', '2022-08-10 10:00:42', 4);
+INSERT INTO `file` VALUES (12, 'profile-fb-adult-01.png', '67d162d71165ddae6f869f73434e43b3.png', '2022-08-12 09:13:13', 4);
+INSERT INTO `file` VALUES (13, 'profile-fb-adult-02.png', 'd6dbc0f346650a02ce984bb9b47794e9.png', '2022-08-12 09:13:13', 4);
+INSERT INTO `file` VALUES (14, 'profile-fb-adult-03.png', '566bbf607f7ec1489ff00c0215e6c4b5.png', '2022-08-12 09:13:13', 4);
+INSERT INTO `file` VALUES (15, 'profile-fb-kid-04.png', '31ba5bd5352d1ed1962b5656c455aa9a.png', '2022-08-12 09:13:13', 4);
+INSERT INTO `file` VALUES (16, 'profile-fb-kid-02.png', '2d370646e8b2d4113db4fb06783463c2.png', '2022-08-12 09:13:13', 4);
+INSERT INTO `file` VALUES (17, 'profile-fb-adult-04.png', '1f90bffab27731b805dd1346353deaaa.png', '2022-08-12 09:13:13', 4);
+INSERT INTO `file` VALUES (18, 'profile-fb-adult-05.png', '728aa4ed5516f36077da36ea7233bd09.png', '2022-08-12 09:13:13', 4);
+INSERT INTO `file` VALUES (19, 'profile-fb-kid-03.png', 'a5b6ecf5be85ceadd6384242018b7559.png', '2022-08-12 09:13:13', 4);
 
 -- ----------------------------
 -- Table structure for task
@@ -48,18 +51,18 @@ CREATE TABLE `task`  (
   `taskname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `tasklevel` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 14 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of task
 -- ----------------------------
-INSERT INTO `task` VALUES (2, '优化接口的防抖与节流', '1');
-INSERT INTO `task` VALUES (3, '升级react-router v6', '1');
-INSERT INTO `task` VALUES (4, '配置vite开发环境和生产环境', '2');
-INSERT INTO `task` VALUES (5, '实现响应式布局，兼容移动设备', '2');
-INSERT INTO `task` VALUES (6, '动态主题颜色，动态换', '2');
-INSERT INTO `task` VALUES (7, '使用TS重写', '3');
-INSERT INTO `task` VALUES (8, '配置自动化更新部署，CI/CD', '3');
+INSERT INTO `task` VALUES (1, '实现响应式布局，兼容移动设备', '3');
+INSERT INTO `task` VALUES (2, '使用TS重写', '2');
+INSERT INTO `task` VALUES (3, '配置自动化更新部署，CI/CD', '2');
+INSERT INTO `task` VALUES (4, '修复在登陆的时候表单默认行为，多次登录才跳转成功', '1');
+INSERT INTO `task` VALUES (5, '大文件断点续传', '3');
+INSERT INTO `task` VALUES (6, '修复邮箱验证', '3');
+INSERT INTO `task` VALUES (13, '国际化', '3');
 
 -- ----------------------------
 -- Table structure for user
@@ -79,23 +82,22 @@ CREATE TABLE `user`  (
   `last_login_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0),
   `last_login_ip` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 54 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 53 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'CC', 'e10adc3949ba59abbe56e057f20f883e', 0, '15657122361', 'hcc960923@gmail.com', '2022-08-04 16:51:35', '489608837435e386d53d419a913340a7.png', 'LKLKLKLKLKLKLKLKLKLKLKLKLKLK', 2, '2022-08-04 16:51:35', '127.0.0.1');
+INSERT INTO `user` VALUES (1, 'CC', 'e10adc3949ba59abbe56e057f20f883e', 0, '15657122361', 'hcc960923@gmail.com', '2022-08-10 15:09:42', '489608837435e386d53d419a913340a7.png', 'LKLKLKLKLKLKLKLKLKLKLKLKLKLK', 2, '2022-08-10 15:09:42', '127.0.0.1');
 INSERT INTO `user` VALUES (2, '曹青青', 'e10adc3949ba59abbe56e057f20f883e', 1, '15157122361', 'hcc96923@gmail.com', '2022-08-04 16:12:05', '9771b1e3eadfad366ec5daefcd37b3f9.png', '', 2, '2022-08-04 16:12:05', '127.0.0.1');
 INSERT INTO `user` VALUES (3, '张家辉', 'e10adc3949ba59abbe56e057f20f883e', 1, '15825759654', 'zjh987@qq.com', '2022-08-10 09:52:08', 'bebeec2dd941b1258d286c6408e2e1bd.png', '', 1, '2022-08-10 09:52:08', '');
-INSERT INTO `user` VALUES (4, '曹操', 'e10adc3949ba59abbe56e057f20f883e', 0, '15657122369', 'hcc96923@163.com', '2022-08-09 16:50:23', '93f913b7884c82712c54ff1a98b07efe.png', '', 3, '2022-08-09 16:50:23', '127.0.0.1');
+INSERT INTO `user` VALUES (4, '曹操', 'e10adc3949ba59abbe56e057f20f883e', 1, '15657122369', 'hcc96923@163.com', '2022-08-12 16:00:49', '93f913b7884c82712c54ff1a98b07efe.png', '', 3, '2022-08-12 16:00:49', '127.0.0.1');
 INSERT INTO `user` VALUES (5, '刘备', 'e10adc3949ba59abbe56e057f20f883e', 0, '18147275149', '734499162@qq.com', '2022-08-04 15:10:40', 'c7a95b58b97f341d9584fe004a07c1b8.png', '', 1, '2022-08-04 15:10:40', '');
-INSERT INTO `user` VALUES (6, '孙权', 'e10adc3949ba59abbe56e057f20f883e', 0, '18147275149', 'hcc96923@gmail.com', '2022-08-04 15:10:47', 'ecb8a0162adad52b748b5de62567a8a7.png', '', 2, '2022-08-04 15:10:47', '');
+INSERT INTO `user` VALUES (6, '孙权', 'e10adc3949ba59abbe56e057f20f883e', 1, '18147275149', 'hcc96923@gmail.com', '2022-08-10 15:04:51', 'ecb8a0162adad52b748b5de62567a8a7.png', '', 2, '2022-08-10 15:04:51', '');
 INSERT INTO `user` VALUES (7, '诸葛亮', 'e10adc3949ba59abbe56e057f20f883e', 0, '15157122361', 'hcc96923@163.com', '2022-08-04 15:10:53', '5bff662e84cbb344c4f2ac0e5ccd7165.png', '', 1, '2022-08-04 15:10:53', '');
 INSERT INTO `user` VALUES (8, '貂蝉', 'e10adc3949ba59abbe56e057f20f883e', 1, '15657122362', '734499162@qq.com', '2022-08-04 15:11:00', 'e1d0a06db9d0af08bad24578610d331a.png', '', 1, '2022-08-04 15:11:00', '');
 INSERT INTO `user` VALUES (49, '张家辉', '123456', 0, '15825759654', 'zjh9811171@qq.com', '2022-08-04 15:24:31', '94022e68963d925e715e2b9a0fa0eccd.png', '', 1, '2022-08-04 15:24:31', '');
 INSERT INTO `user` VALUES (50, '张家辉', '123456', 0, '15825759654', 'hcc096923@163.com', '2022-08-04 15:24:35', '8b018f203e07a9eb37f7a179b919e7d0.png', '', 1, '2022-08-04 15:24:35', '');
-INSERT INTO `user` VALUES (51, '小乔s', '123456', 0, '15157122361', 'hcc96923@gmail.com', '2022-08-04 15:49:29', 'e308b36ca5d021df45bd2f80bfc20201.png', '', 1, '2022-08-04 15:49:29', '');
-INSERT INTO `user` VALUES (52, 'CCCf', '123456', 0, '15157122361', 'zjh981117@qq.com', '2022-08-04 15:16:49', '7ad4f474de0a9470f3b929992b24506f.png', '', 1, '2022-08-04 15:16:49', '');
-INSERT INTO `user` VALUES (53, 'xxxxx', '123456', 1, '15657122361', 'hcc960923@gmail.com', '2022-08-04 15:20:09', '5463611981d4800d7c70ec12b651de3d.png', '', 1, '2022-08-04 15:20:09', '');
+INSERT INTO `user` VALUES (51, '小乔', '123456', 1, '15157122361', 'hcc96923@gmail.com', '2022-08-10 15:35:37', 'e308b36ca5d021df45bd2f80bfc20201.png', '', 1, '2022-08-10 15:35:37', '');
+INSERT INTO `user` VALUES (52, '', 'undefined', 0, '', '15657122362@163.com', '2022-08-12 17:27:02', '', '', 1, '2022-08-12 17:27:02', '');
 
 SET FOREIGN_KEY_CHECKS = 1;
