4cfe219a7fb359d6c2c6d2dd3024b4f5683137af
hcc960923 <101438085+hcc960923@users.noreply.github.com>
2022-08-30 18:01:38 +0800

    CCC

---
+++ vite.config.ts
@@ -0,0 +1,54 @@
+import { resolve } from "path";
+import { defineConfig, loadEnv, ConfigEnv, UserConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import viteCompression from "vite-plugin-compression";
+import { createStyleImportPlugin, AntdResolve } from "vite-plugin-style-import";
+
+export default defineConfig((mode: ConfigEnv): UserConfig => {
+  const configEnv = loadEnv(mode.mode, "./");
+  
+  return {
+    resolve: {
+			alias: {
+				"@": resolve(__dirname, "./src")
+			}
+		},
+    css: {
+			preprocessorOptions: {
+				less: {
+					javascriptEnabled: true,
+				}
+			}
+		},
+    server: {
+      host: configEnv.VITE_HOST,
+      port:  Number(configEnv.VITE_PORT),
+      https: false
+    },
+    plugins: [
+      reactRefresh(),
+      createStyleImportPlugin({ resolves: [AntdResolve()] }),
+      viteCompression({
+        verbose: true,
+        disable: false,
+        threshold: 10240,
+        algorithm: "gzip",
+        ext: ".gz",
+      }),
+    ],
+    esbuild: {
+			pure: configEnv.VITE_HOST === "production" ? ["console.log", "debugger"] : []
+		},
+    build: {
+      outDir: configEnv.VITE_OUTPUT_DIR,
+      minify: "esbuild",
+      rollupOptions: {
+        output: {
+          chunkFileNames: "js/[name]-[hash].js",
+          entryFileNames: "js/[name]-[hash].js",
+          assetFileNames: "[ext]/[name]-[hash].[ext]",
+        },
+      },
+    },
+  }
+});
